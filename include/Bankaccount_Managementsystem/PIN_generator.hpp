9db1b9a04e5ae7433ed1b23e1c5be2c9c420d379
Florian Meier <flm0367@thi.de>
2021-05-15 22:59:25 +0200

    Implemented PIN generator

---
+++ include/Bankaccount_Managementsystem/PIN_generator.hpp
@@ -0,0 +1,18 @@
+/**
+ *  This code is owned by Florian Meier
+ */
+
+typedef char uint8_t;
+
+class PIN_generator
+{
+    private:
+        uint8_t PIN;                // Generated PIN
+
+    public:
+        void generatePin();         // Generate PIN according to current time
+        uint8_t getPIN();           // Getter for generated PIN
+
+        PIN_generator();            // Default constructor
+        ~PIN_generator();           // Destructor
+};
---
+++ src/PIN_generator.cpp
@@ -0,0 +1,30 @@
+/**
+ *  This code is owned by Florian Meier
+ */
+
+#include <ctime>
+#include <cstdlib>
+#include "include/Bankaccount_Managementsystem/PIN_generator.hpp"
+
+PIN_generator::PIN_generator()
+{
+    PIN = 0;                                            
+}
+
+void PIN_generator::generatePin()
+{
+    PIN = 0;
+
+    for(uint8_t i = 1; i <= 1000; i *= 10)               // Since the PIN must have 4 digits 
+    {
+        std::srand(std::time(0));                       // Use current time as seed for random generator
+        uint8_t random = std::rand() % 10;              // Get a number between 0 and 9 from the random number       
+
+        PIN += random * i;                              // Apply decimal factor to get a 4 digit PIN
+    }    
+}
+
+uint8_t PIN_generator::getPIN()
+{
+    return PIN;
+}
